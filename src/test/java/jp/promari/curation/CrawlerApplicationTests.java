8eb516d11317b7bed32672b969f5ac8a85641e0f
takaomi.murasaki@promari.jp <takaomi.murasaki@promari.jp>
2020-03-29 05:40:53 +0900

    add CrawlerApplication

---
+++ src/test/java/jp/promari/curation/CrawlerApplicationTests.java
@@ -0,0 +1,39 @@
+package jp.promari.curation;
+
+import jp.promari.curation.web.service.ChapterService;
+import jp.promari.curation.web.service.ContentService;
+import jp.promari.curation.web.service.NovelService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrawlerApplicationTests {
+
+    private Logger logger = LoggerFactory.getLogger(CrawlerApplicationTests.class);
+
+    @Autowired
+    private NovelService novelService;
+    @Autowired
+    private ChapterService chapterService;
+    @Autowired
+    private ContentService contentService;
+
+    @Test
+    public void testService(){
+        novelService.findAll().forEach(item -> logger.info("Query test：{}",item));
+        chapterService.findAll().forEach(item -> logger.info("Query test：{}",item));
+        contentService.findAll().forEach(item -> logger.info("Query test：{}",item));
+    }
+
+
+    @Test
+    public void testLogger() {
+        logger.info("Test log");
+    }
+
+
+}
