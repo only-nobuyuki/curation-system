d1fd419303de6f96581fda6781b56d5e157bf75e
takaomi.murasaki@promari.jp <takaomi.murasaki@promari.jp>
2020-03-28 19:49:21 +0900

    update Test

--- src/test/java/jp/co/promari/curation/web/SimpleWebCrawlerTest.java
+++ src/test/java/jp/co/promari/curation/web/SimpleWebCrawlerTest.java
@@ -11,9 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-@Profile("test")
 public class SimpleWebCrawlerTest {
 
     @Test
