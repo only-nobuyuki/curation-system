b71310018adf24876affd6be836da8a169ba7892
takaomi.murasaki@promari.jp <takaomi.murasaki@promari.jp>
2020-04-18 00:54:33 +0900

    不要なimport文の削除

--- src/main/java/jp/promari/curation/PromariCurationApp.java
+++ src/main/java/jp/promari/curation/PromariCurationApp.java
@@ -1,13 +1,9 @@
 package jp.promari.curation;
 
-
-import jp.promari.curation.web.crawler.novel.task.NovelProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import us.codecraft.webmagic.Page;
-import us.codecraft.webmagic.processor.PageProcessor;
 
 @SpringBootApplication
 @EnableWebMvc
